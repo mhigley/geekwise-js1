cecabd75a5465042e8f5d4f4185dbf07ca60e58f
Matthew Higley <matthewhigley@MattyBook-Pro.local>
2017-05-15 20:57:31 -0700

    day01

---
+++ day01/js/script.js
@@ -0,0 +1,65 @@
+// ES6 - ECMAScript6
+
+// Good
+var lowercase;
+var camelCase;
+var use_underscores;
+var _underscores;
+var $dollarsign;
+
+// Meh
+var SuperCamelCase;
+
+// Bad
+// var no spaces;
+// var no-hyphens;
+// var 1nope;
+
+console.log('Sending a message to the console.');
+console.info('Sending an information message');
+console.warn('Heres a warning');
+console.error('Danger Will Robinson!');
+
+// console.log();
+
+// Single Line Comment
+/*
+    Multi
+    Line
+    Comments
+*/
+
+var
+
+
+
+                                    freedom
+
+
+
+        =
+
+
+
+
+
+
+
+        "A string of" +
+        " " +
+        "Javascript";
+
+console.log(freedom);
+
+// template string
+
+var javascriptTemplateString = `
+    and
+                        type
+    whatever
+you
+                            want
+    :)
+`;
+
+console.log(javascriptTemplateString);
